3026f6b36fdb48f01a8c87eb2ab453b86a7fc5c2
ReidHoneycutt <30945205+ReidHoneycutt@users.noreply.github.com>
2023-04-30 01:34:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,22 +5,35 @@ import random
 from cvzone.HandTrackingModule import HandDetector
 import math
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 720)
 
-# HAND DETECTOR
-detector = HandDetector(detectionCon=0.8, maxHands=1)
+def init_cap():
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 1280)
+    cap.set(4, 720)
+    return cap
+
+def init_detector():
+    detector = HandDetector(detectionCon=0.8, maxHands=1)
+    return detector
 
 
 
-while True:
-    success, img = cap.read()
-    hands, img = detector.findHands(img)
+def move_cursor(cap, detector):
+    while True:
+        success, img = cap.read()
+        hands, img = detector.findHands(img)
 
-    if hands:
-        bbox = hands[0]['bbox']
-        #cv2.imshow("Image", img)
-        cv2.waitKey(1)
-        coor = str(hands[0]["lmList"][8][:-1][0])+","+str(hands[0]["lmList"][8][:-1][1])
-        pyautogui.moveTo(1512-hands[0]["lmList"][8][:-1][0], hands[0]["lmList"][8][:-1][1])
+        if hands:
+            pyautogui.moveTo(1920-(7/6)*hands[0]["lmList"][8][0], (1/2)*hands[0]["lmList"][8][1])
+
+        cv2.waitKey(0)
+
+
+def main():
+    cap = init_cap()
+    detector = init_detector()
+    move_cursor(cap, detector)
+
+
+if __name__ == '__main__':
+    main()
